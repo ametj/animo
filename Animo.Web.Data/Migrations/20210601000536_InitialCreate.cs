f1dc11ed84f707f8446f201b0cc5293b3eab274a
Matej Leško <lesko.matej@outlook.com>
2021-06-01 15:19:05 +0200

    Upgrade to .Net6

--- Animo.Web.Data/Migrations/20210601000536_InitialCreate.cs
+++ Animo.Web.Data/Migrations/20210601000536_InitialCreate.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
-using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Animo.Web.Data.Migrations
 {
