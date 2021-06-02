00c5e0601ba60683b609035e892735a85ff68364
Matej Leško <lesko.matej@outlook.com>
2021-06-03 00:44:11 +0200

    Delete database before running tests

--- Animo.Web.Tests.Integration/CustomWebApplicationFactory.cs
+++ Animo.Web.Tests.Integration/CustomWebApplicationFactory.cs
@@ -1,7 +1,9 @@
 using Animo.Web.Api;
+using Animo.Web.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System.IO;
 
 namespace Animo.Web.Tests.Integration
@@ -16,6 +18,19 @@ namespace Animo.Web.Tests.Integration
             builder.ConfigureAppConfiguration((context, conf) =>
             {
                 conf.AddJsonFile(configPath);
+            });
+
+            builder.ConfigureServices(services =>
+            {
+                // Build the service provider.
+                var serviceProvider = services.BuildServiceProvider();
+
+                // Create a scope to obtain a reference to the database context.
+                using var scope = serviceProvider.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<AnimoDbContext>();
+
+                // Delete database before running tests.
+                db.Database.EnsureDeleted();
             });
         }
     }
