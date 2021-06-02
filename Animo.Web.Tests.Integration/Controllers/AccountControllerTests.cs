8921f67c5411e642b818083d1033dd3b4db9acd8
Matej Leško <lesko.matej@outlook.com>
2021-06-03 01:09:45 +0200

    Add login token tests

--- Animo.Web.Tests.Integration/Controllers/AccountControllerTests.cs
+++ Animo.Web.Tests.Integration/Controllers/AccountControllerTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Net;
+﻿using Animo.Web.Core.Dto;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -13,15 +16,44 @@ namespace Animo.Web.Tests.Integration.Controllers
         [Fact]
         public async Task Login_ExistingUser_ShouldLogin()
         {
-            var responseLogin = await LoginAsAdminUserAsync();
-            Assert.Equal(HttpStatusCode.OK, responseLogin.StatusCode);
+            var response = await LoginAsAdminUserAsync();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
 
         [Fact]
         public async Task Login_NonExistingUser_ShouldNotLogin()
         {
-            var responseLogin = await Login("123_aBc", "123_aBc");
-            Assert.Equal(HttpStatusCode.BadRequest, responseLogin.StatusCode);
+            var response = await Login("123_aBc", "123_aBc");
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Login_ExistingUser_ShouldGetToken()
+        {
+            var response = await LoginAsMemberUserAsync();
+            var result = await response.Content.ReadAsAsync<LoginToken>();
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotNull(result.Token);
+        }
+
+        [Fact]
+        public async Task Login_Token_ShouldAllowAccess()
+        {
+            var loginResponse = await LoginAsAdminUserAsync();
+            var loginResult = await loginResponse.Content.ReadAsAsync<LoginToken>();
+            var token = loginResult.Token;
+            
+            Assert.Equal(HttpStatusCode.OK, loginResponse.StatusCode);
+            Assert.NotNull(loginResult.Token);
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "/api/users");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var client = _factory.CreateClient();
+            var response = await client.SendAsync(request);
+            
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
     }
 }
